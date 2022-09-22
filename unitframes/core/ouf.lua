e5217a52844ee9f38492e24d39ebe179adcdcbfd
Niitek <67418764+Niitek@users.noreply.github.com>
2022-09-22 05:25:55 -0500

    LuaCheck: update to fix errors

--- unitframes/core/ouf.lua
+++ unitframes/core/ouf.lua
@@ -10,6 +10,7 @@ local argcheck = Private.argcheck
 local print = Private.print
 local error = Private.error
 
+---@diagnostic disable-next-line: unbalanced-assignments
 local styles, style = {}
 local callback, objects, headers = {}, {}, {}
 
@@ -533,11 +534,11 @@ do
 		end
 
 		if(visibility) then
-			local type, list = string.split(' ', visibility, 2)
+			local type, list = strsplit(' ', visibility, 2)
 			if(list and type == 'custom') then
 				RegisterAttributeDriver(header, 'state-visibility', list)
 			else
-				local condition = getCondition(string.split(',', visibility))
+				local condition = getCondition(strsplit(',', visibility))
 				RegisterAttributeDriver(header, 'state-visibility', condition)
 			end
 		end
