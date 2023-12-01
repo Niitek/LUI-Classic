4c2b99b0483df5de706f7db8df290d442813b8ad
Niitek <67418764+Niitek@users.noreply.github.com>
2023-11-30 22:13:21 -0600

    Core: updating version.

--- init.lua
+++ init.lua
@@ -8,9 +8,4 @@ LUI.Lib = _G.LibStub
 
 LUI.Lib("AceAddon-3.0"):NewAddon(LUI, addonname, "AceComm-3.0", "AceConsole-3.0", "AceEvent-3.0", "AceHook-3.0")
 LUI.L = LUI.Lib("AceLocale-3.0"):GetLocale(addonname)
-LUI.Rev = "v3.51.0"
-
-LUI.IsRetail = (_G.WOW_PROJECT_ID == _G.WOW_PROJECT_MAINLINE)
-LUI.IsWrath = (_G.WOW_PROJECT_ID == _G.WOW_PROJECT_WRATH_CLASSIC)
-LUI.IsBCC = (_G.WOW_PROJECT_ID == _G.WOW_PROJECT_BURNING_CRUSADE_CLASSIC)
-LUI.IsClassic = (_G.WOW_PROJECT_ID == _G.WOW_PROJECT_CLASSIC)
+LUI.Rev = "v3.52.0-SoD"
